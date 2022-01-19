577486b80ae7709618ec66070385fc56f1fea24b
Peter Forrest <petergforrest91@gmail.com>
2022-01-19 16:21:29 -0600

    fixing tabs

--- Program.cs
+++ Program.cs
@@ -13,32 +13,33 @@ namespace consoleTester
             string input = Console.ReadLine();
             string reversed = StrRevFunction(input);
             do {
-              if (input == StrRevFunction(input))
-              {
-                Console.WriteLine($"{iterations} {input}: Palindrome");
-              }
-              else
-              {
-                long inputNumber = Convert.ToInt64(input);
-                Console.WriteLine($"Input: {inputNumber}, Reversed: {StrRevFunction(input)}");
-                long addedResult = inputNumber + Convert.ToInt64(StrRevFunction(input));
-                Console.WriteLine($"Added result: {addedResult}");
-                string nextNumber = addedResult.ToString();
-                input = nextNumber;
-                iterations++;
-              }
+                if (input == StrRevFunction(input))
+                {
+                  Console.WriteLine($"{iterations} {input}: Palindrome");
+                }
+                else
+                {
+                  long inputNumber = Convert.ToInt64(input);
+                  Console.WriteLine($"Input: {inputNumber}, Reversed: {StrRevFunction(input)}");
+                  long addedResult = inputNumber + Convert.ToInt64(StrRevFunction(input));
+                  Console.WriteLine($"Added result: {addedResult}");
+                  string nextNumber = addedResult.ToString();
+                  input = nextNumber;
+                  iterations++;
+                }
             } while (input != StrRevFunction(input));
             Console.WriteLine($"{iterations} {input}");
         }
 
-        public static string StrRevFunction(string inputStr) {
-          char [] reversed = new char[inputStr.Length];
-          int moveup = 0;
-          for (var j = (inputStr.Length - 1); j >= 0; j--)
-          {
-              reversed[moveup++] = inputStr[j];
-          }
-          return new string(reversed);
+        public static string StrRevFunction(string inputStr)
+        {
+            char [] reversed = new char[inputStr.Length];
+            int moveup = 0;
+            for (var j = (inputStr.Length - 1); j >= 0; j--)
+            {
+                reversed[moveup++] = inputStr[j];
+            }
+            return new string(reversed);
         }
     }
 }
